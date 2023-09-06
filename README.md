f2e04af233a747d14767943fcbd8394ed8f72093
kgsanchez012 <kgsanchez012@gmail.com>
2023-09-05 21:24:47 -0400

    add README and initial display module

---
+++ README.md
@@ -0,0 +1,1 @@
+The goal of this assignment is to create a version of the game Mastermind in Ruby. Completing this game will demonstrate the basics of Object Oriented Programming through the use of classes, modules, and methods.
---
+++ display.rb
@@ -0,0 +1,36 @@
+module Display
+    def display_intro
+        "Welcome to Mastermind! \n\n"
+    end
+
+    def display_role(role)
+        "You are the #{role}!"
+    end
+
+    def display_decoder_start
+        "Try to guess the four-color code: [?, ?, ?, ?].
+        \nYour hints are X for a correct color and location and O for a correct color but wrong location."
+    end
+
+    def display_coder_start
+        "Choose four letters to define the colors and order of your secret code, then the decoder will try to guess it.
+        \nR = Red, O = Orange, Y = Yellow, G = Green, B = Blue, V = Violet."
+    end
+
+    def display_feedback(guess, hint, turns)
+        "Your guess was: #{guess}. Your hint is #{hint}. There are #{turns} turns left!
+        \n\nTry to guess the code: [?, ?, ?, ?]..."
+    end
+
+    def display_input_warning
+        "The code cannot have duplicate colors. Please try again."
+    end
+
+    def display_win
+        "The decoder has guessed the code correctly and won! Game over!"
+    end
+
+    def display_lose
+        "The decoder has failed to guess the code and lost. Game over."
+    end
+end
