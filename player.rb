3d323cbf2be65cfd8aacb89c673d0d62e2e59c3b
kgsanchez012 <kgsanchez012@gmail.com>
2023-09-05 22:02:01 -0400

    add player class

---
+++ player.rb
@@ -0,0 +1,16 @@
+class Player
+    attr_reader :name, :role
+
+    def initialize(name, role)
+        @name = name
+        @role = role
+    end
+end
+
+class Human > Player
+    #
+end
+
+class Computer > Player
+    #
+end
